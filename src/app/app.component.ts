b252eb130e52f526383fa78fbeeee6411b217350
daTaHere <adamhuy123@yahoo.com>
2024-10-05 11:48:27 -0700

    updated detail-page view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { HousingLocationsComponent } from './housing-locations/housing-locations.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -8,15 +8,15 @@ import { HousingLocationsComponent } from './housing-locations/housing-locations
   template: `
     <main>
       <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" routerLink="">
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent,HousingLocationsComponent],
+  imports: [HomeComponent,RouterModule],
 })
 export class AppComponent {
   title = 'homes';
